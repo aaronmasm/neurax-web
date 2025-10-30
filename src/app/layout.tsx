44b108aa741381e217758a43b0b0eb4d4ec2a4af
Aarón Más <aaronmasm85@gmail.com>
2025-10-29 22:21:31 -0500

    feat(branding): installed project fonts and defined primary brand colors from brandbook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,32 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
+import { Roboto, Inter } from "next/font/google";
 import "./globals.css";
 
+// NeuraX base fonts
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["700"],
+  variable: "--font-inter",
+}); // Replacement for Helvetica Neue Bold
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-roboto",
+});
+
 export const metadata: Metadata = {
-  title: "Tu Proyecto", // Cambia esto por el nombre de tu app
-  description: "Descripción de tu proyecto",
+  title: "NeuraX | Tech, Design & Evol",
+  description: "Soluciones tecnológicas inteligentes y futuristas.",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es-PE">
-      <body className="antialiased">{children}</body>
+      <body className={`${inter.variable} ${roboto.variable} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
